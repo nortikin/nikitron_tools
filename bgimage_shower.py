452864b923d91e8d53b1ca946a04c10adfa63302
nortikin <nikitronn@yandex.ru>
2017-10-09 00:11:01 +0300

    bgimage as addon

--- bgimage_shower.py
+++ bgimage_shower.py
@@ -1,3 +1,38 @@
+ # -*- coding: utf-8 -*-
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+
+bl_info = {
+    "name": "Nikitron tools",
+    "version": (0, 1, 0),
+    "blender": (2, 7, 9),  
+    "category": "Camera",
+    "author": "Nikita Gorodetskiy",
+    "location": "object",
+    "description": "camera background changer",
+    "warning": "",
+    "wiki_url": "",          
+    "tracker_url": "http://www.blenderartists.org/forum/showthread.php?272679-Addon-WIP-Sverchok-parametric-tool-for-architects",  
+}
+
+
+
 import bpy
 from bpy.props import StringProperty, CollectionProperty
 import os
