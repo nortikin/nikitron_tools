b8c3ea1d6ea68c22519ec2c5e24ed0a49e706c3e
nortikin <nikitronn@yandex.ru>
2022-06-10 00:15:18 +0300

    spoiler

--- pythonism/text_spoiler.py
+++ pythonism/text_spoiler.py
@@ -1,24 +1,49 @@
 import re
 import random as r
+import sys
 
-messness = 8 # define every 5th glitched letter
+messness = 8 # define every n-th glitched letter
 lib = 'а б в г д е ё ж з и й к л м н о п р с \
        т у ф х ц ч ш щ ъ ы ь э ю я А Б В Г Д \
        Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц \
        Ч Ш Щ Ъ Ы Ь Э Ю Я \
        - = ! " № ; % : ? * ( ) _ +'.split()
-text = '/home/ololo/glitch'
-a = open(text)
-b = a.read()
-a.close()
-c = b.splitlines()
-e = [i.split() for i in c if i]
-out = ''
-for i in e:
-    y = list(' '.join(i))+['\n']
-    n = len(y)-2
-    m = [r.randint(0,n-1) for k in range(n//messness)]
-    for t in m:
-        y[t] = r.choice(lib)
-    out += ''.join(y)
-print(out)
+
+def spoil(b):
+    # основная функция, портит как может
+    c = b.splitlines()
+    e = [i.split() for i in c if i]
+    out = ''
+    for i in e:
+        y = list(' '.join(i))+['\n']
+        n = len(y)-2
+        m = [r.randint(0,n-1) for k in range(n//messness)]
+        for t in m:
+            y[t] = r.choice(lib)
+        out += ''.join(y)
+    print(out)
+
+def spoilfile(file_):
+    # только читает файл
+    a = open(file_)
+    b = a.read()
+    a.close()
+    out = spoil(b)
+
+if __name__ == '__main__':
+    sa = sys.argv
+    if len(sa) > 1:
+        if sa[1] == '-f':
+            if len(sa)>2:
+                spoilfile(sa[2])
+            else:
+                file_ = input('file path:')
+                spoilfile(file_)
+        else:
+            text = sa[1:]
+            print()
+            spoil(' '.join(text))
+    else:
+        print('usage: python3 text_spoiler.py -f <filepath>')
+        print('   or: python3 text_spoiler.py <your text here>')
+
